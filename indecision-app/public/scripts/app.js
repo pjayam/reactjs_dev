4771ed0223ee57e5a2ebe558898e790f1dc9cde3
Pavan <pjayam@marketo.com>
2019-01-23 18:42:17 +0530

    ex

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,9 +3,18 @@
 console.log('App.js is Running');
 //JSX  Javascript - XML
 var template = React.createElement(
-  'h1',
-  null,
-  'JSX Expression.....'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        ' JSX Expression..... '
+    ),
+    React.createElement(
+        'p',
+        null,
+        'paragraph'
+    )
 );
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,10 @@
 console.log('App.js is Running')
 //JSX  Javascript - XML
-var template = <h1>JSX Expression.....</h1>;
+var template = (
+<div> 
+    < h1 > JSX Expression..... </h1>
+    <p>paragraph</p > 
+</div>
+);
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
